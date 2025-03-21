5f61b0ed9d2bcde9b864157d5bddc4f6bca47505
VISHNU <vishnuwork5451@gmail.com>
2025-03-21 12:18:13 +0530

    new trying

---
+++ src/Pen.java
@@ -0,0 +1,17 @@
+
+public class Pen {
+        String name = "inkpen";
+        String colour = "bluecolour";
+        int rate = 20;
+        //writingTheBook()
+        void writingTheBook()
+        {
+            System.out.println("I want inkpen with bluecolour for writing the book");
+
+        }
+    public static void main(String[] args) {
+            Pen pen = new Pen();
+            pen.writingTheBook();
+    }
+    }
+
---
+++ src/exmp001/calculator.java
@@ -0,0 +1,11 @@
+package exmp001;
+
+public class calculator {
+    public static void main(String[] args){
+        System.out.println(10+1);
+        System.out.println(10-3);
+        System.out.println(20*4);
+        System.out.println(30/8);
+
+    }
+}
