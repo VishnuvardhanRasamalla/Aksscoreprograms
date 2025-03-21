77b0db26e65b1e935866645f37a6ef640e651621
VISHNU <vishnuwork5451@gmail.com>
2025-03-21 12:49:33 +0530

    practice

---
+++ src/exmp001/examp2.java
@@ -0,0 +1,27 @@
+package exmp001;
+
+import java.sql.SQLOutput;
+
+public class examp2 {
+    public static void main(String[] args){
+        int a = 30;
+        int b = 40;
+        int c = 35;
+        if(a>b && b<c)
+        {
+            System.out.println("a is bigger");
+        }
+        else if (a<b && b>c)
+        {
+            System.out.println("B is bigger");
+        }
+            else
+        {
+            System.out.println("not valid");
+        }
+
+
+    }
+
+
+}
